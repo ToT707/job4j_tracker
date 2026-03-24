cb4e238cf543d41f0cca64a524a74377efc35501
ToT707 <easy0894@gmail.com>
2026-03-24 16:32:14 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,7 +2,7 @@ package ru.job4j.tracker;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item(1, "qaz");
+        Item item = new Item(1, "qas");
         System.out.println(item);
     }
 }
